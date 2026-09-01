7212e0ade2cb26454e21de386d80b6db459ac765
zabe-dev <jay@zabe.dev>
2026-09-01 15:47:43 +0800

    fix: match coin page ad banner to homepage

--- features/coin/components/coin-ad.tsx
+++ features/coin/components/coin-ad.tsx
@@ -2,12 +2,12 @@ import Link from 'next/link';
 
 export function CoinAd() {
   return (
-    <div className="container coin-ad">
-      <small>ADVERTISEMENT</small>
-      <span>
-        <b>Reach crypto&apos;s earliest coin hunters.</b> Premium inventory · Measured impressions
-        and clicks
-      </span>
+    <div className="container wide-banner">
+      <small>FULL-WIDTH ADVERTISEMENT</small>
+      <div>
+        <b>Reach crypto&apos;s earliest coin hunters.</b>
+        <span>Premium inventory · Measured impressions and clicks</span>
+      </div>
       <Link href="/advertise">View ad packages ↗</Link>
     </div>
   );
